dfa93e235329849b3b7860ce76db642e0c55d5d8
Nam Vo <vhnam2504@gmail.com>
2019-05-04 15:31:59 +0700

    feat(Home): complete Home page

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,7 +1,65 @@
 import React from 'react';
 
+import config from '../../config';
+
+import Button from '../../components/Button';
+import Typography, {
+  TypoWeight,
+  TypoVariants,
+} from '../../components/Typography';
+
+import imgPrototypingProcess from '../../assets/img/prototyping-process.svg';
+
+import styles from './Home.module.css';
+
 const Home = () => {
-  return <div>This is homepage</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <div className={styles.wrapper}>
+            <div className={styles.container}>
+              <Typography
+                className={styles.heading}
+                weight={TypoWeight.semibold}
+              >
+                Inside
+                <br />
+                out
+              </Typography>
+              <Typography
+                className={styles.subHeading}
+                variant={TypoVariants.h2}
+                weight={TypoWeight.light}
+              >
+                International conference <br /> on illusstraion
+              </Typography>
+              <Typography
+                className={styles.location}
+                variant={TypoVariants.h4}
+                weight={TypoWeight.semibold}
+              >
+                2019, September 25 - 28
+                <br /> VNU University of Science, HCMC
+              </Typography>
+              <div className={styles.btnRegister}>
+                <Button href={config.paths.registration}>Register Now</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className={styles.wrapper}>
+            <img
+              className={styles.illustration}
+              src={imgPrototypingProcess}
+              alt="Prototyping Process"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
